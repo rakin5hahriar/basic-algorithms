a8910ac3580eef89639f91e9b46c7900837728ff
rakin <shahriar.rakin99@gmail.com>
2024-05-01 17:18:37 +0600

    basic input output

---
+++ STL/input.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    int id;
+
+    cout << "information" << endl;
+    // taking input
+    cin>> name >> id;
+
+    cout << "name:" << name << " " << "id:" << " " << id << endl;
+
+
+    return 0;
+}
---
+++ STL/output.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // general text print
+    cout << "text printing" << endl;
+
+
+    //taking variable and assigning value
+    string name = "rakin";
+    int id = 16;
+    char section = 'B';
+
+    //printing value taken in variable
+    cout << "name" << " " << name << " " << "id" << " " << id << " " << "sec" << " " << section;
+    
+
+
+    return 0;
+}
