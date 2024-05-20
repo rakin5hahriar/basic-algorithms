5d7d3f729a406472d0c52a34e21918ec06a65017
rakins <shahriar.rakin99@gmail.com>
2024-05-20 12:18:04 +0600

    vector access using iterator

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -15,9 +15,9 @@ int main(){
     // // vector e data 2 vabe deya jay
     // // 1. array 2. vector
 
-    // // array er maddhome
-    // int arr[] = {1, 2, 3, 4, 5};
-    // vector<int> v2(arr, arr+5);
+    // array er maddhome
+    int arr[] = {1, 2, 3, 4, 5};
+    vector<int> v2(arr, arr+5);
 
     // cout << v2.size() << endl; 
 
@@ -32,21 +32,28 @@ int main(){
     // }
     // cout << endl;
 
-    // value insertion in v1
-    int n;
-    cin >> n;
-    for (int i =0; i<=n; i++){
-        int m;
-        cin >> m;
-        v1.push_back(m);
-    }
-    v1.pop_back();
+    // // value insertion in v1
+    // int n;
+    // cin >> n;
+    // for (int i =0; i<=n; i++){
+    //     int m;
+    //     cin >> m;
+    //     v1.push_back(m);
+    // }
+    // v1.pop_back();
     
     
-    for(int i =0; i < v1.size(); i++){   
-        cout << v1[i] << " ";
+    // for(int i =0; i < v1.size(); i++){   
+    //     cout << v1[i] << " ";
+    // }
+    // cout << endl ;
+
+    // using alternate way to print: iterate using pointer
+    vector<int> :: iterator it;
+    for(it = v2.begin(); it!=v2.end(); it++){
+        cout<< *it << " ";
     }
-    cout << endl  ;
+    cout << endl;
 
 
 
